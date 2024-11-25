e256745bc4aa87f41b33c9e639fe8ce7dd554ef3
Daria Bogatiriov <daria.bogatiriov@carasent.com>
2024-11-25 18:54:09 +0200

    feat: Spring background

--- src/modules/home/components/InvitationImage.tsx
+++ src/modules/home/components/InvitationImage.tsx
@@ -20,7 +20,18 @@ export const InvitationImage = () => {
 
     return (
         <div className='w-full h-full z-[20] relative'>
-            <img src={invitation} alt='invitation' className='w-full h-full min-h-[200px] max-h-[400px] object-cover' />
+            <div className='relative w-full h-full'>
+                <img
+                    src={invitation}
+                    alt='invitation'
+                    className='w-full h-full min-h-[200px] max-h-[400px] object-cover shadow-sm'
+                />
+                <div className='great-vibes-regular absolute top-[10px] left-[60px] w-full text-[#c0822b] text-4xl p-4 text-center'>
+                    А теперь пребывают сии три: вера, надежда, любовь; <br /> но любовь из них больше.
+                    <div className='flex items-end justify-center text-xl '> 1 Коринфянам 13:13</div>
+                </div>
+            </div>
+
             <div className='flex gap-2 absolute bottom-2 right-2'>
                 <button
                     className={cn('btn', i18n.language === 'en' ? 'btn-primary custom-button-bg' : 'btn-secondary')}
